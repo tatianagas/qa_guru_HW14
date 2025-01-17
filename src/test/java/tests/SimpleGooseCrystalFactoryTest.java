b9a480c6d347e642c0359f79ccc73d60f870396a
Tatiana Gasilina <ernolova@gmail.com>
2025-01-17 14:17:45 +0300

    add allure report and selenoid

--- src/test/java/tests/SimpleGooseCrystalFactoryTest.java
+++ src/test/java/tests/SimpleGooseCrystalFactoryTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -9,65 +11,125 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.bidi.script.LocalValue.setValue;
+import static io.qameta.allure.Allure.step;
+
 
 @Tag("goose-crystal")
 public class SimpleGooseCrystalFactoryTest extends TestBase {
+
+    private static final String TEXT = "test";
+    private static final String ALERT = "Неверный логин или пароль.";
 
 
 
     @Test
     @Description("Наличие ссылки на логотип завода на главной странице")
     void logoLinkMainPageTest() {
-        open("/");
-        $("[src=\"/local/templates/goose/images/svg/logotype.svg?v2\"]").shouldBe(visible);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу ", () -> {
+            open("/");
+        });
+        step("Ищем ссылку на лого завода ", () -> {
+            $("[src=\"/local/templates/goose/images/svg/logotype.svg?v2\"]").shouldBe(visible);
+        });
 
     }
 
     @Test
     @Description("Находим рыбницу по фильтрацией в каталоге")
     void findPlateForFishByFiltersTest() {
-        open("/catalog");
-        $$(".vi-tools__filter-button-name")
-                .findBy(text("Наши предложения "))
-                .click();
-        $("[title=Новинка]").click();
-        $$(".vi-tools__filter-button-name")
-                .findBy(text("Коллекция "))
-                .click();
-        $("[title=Даймонд]").click();
-        $(".vi-product__item-title").shouldHave(text("Рыбница, коллекция \"Даймонд\" (бесцветный)"));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем страницу Каталог ", () -> {
+            open("/catalog");
+        });
+
+        step("Выбираем Новинка в фильтре Наши предложения ", () -> {
+            $$(".vi-tools__filter-button-name")
+                    .findBy(text("Наши предложения "))
+                    .click();
+            $("[title=Новинка]").click();
+        });
+
+        step("Выбираем Даймонд в фильтре Коллекция ", () -> {
+            $$(".vi-tools__filter-button-name")
+                    .findBy(text("Коллекция "))
+                    .click();
+            $("[title=Даймонд]").click();
+        });
+
+        step("Проверяем, что среди отфильтрованного есть бесцветная рыбница ", () -> {
+            $(".vi-product__item-title").shouldHave(text("Рыбница, коллекция \"Даймонд\" (бесцветный)"));
+        });
 
     }
 
     @Test
     @Description("На странице конкретной экскурсии есть 2 другие экскурсии")
     void onIndividualTourPageSholdHasOthersToursTest() {
-        open("/ekskursii-na-proizvodstvo");
-        $(withTagAndText("b", "Индивидуальная экскурсия «Друзья в гостях у Хрустального Гуся»")).click();
-        $$(".vilka-excursion-item").shouldHave(size(2));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем страницу экскурсий ", () -> {
+            open("/ekskursii-na-proizvodstvo");
+        });
+
+        step("Выбираем индивидуальную экскурсию ", () -> {
+            $(withTagAndText("b", "Индивидуальная экскурсия «Друзья в гостях у Хрустального Гуся»")).click();
+        });
+
+        step("Проверяем наличие блоков двух других экскурсий на странице ", () -> {
+            $$(".vilka-excursion-item").shouldHave(size(2));
+        });
+
+
     }
 
     @Test
     @Description("Проверяем адрес магазина в Суздале")
     void validSuzdalStoreAddressTest() {
-        open("/about/shops/");
-        $(".vi-buy-pick__item-name-default").click();
-        $("[data-city=suzdal]").click();
-        $$(".vi-buy-city__block-shop-contact")
-                .findBy(text("Адрес"))
-                .sibling(0)
-                .shouldHave(text("601293, Владимирская область, г. Суздаль, ул. Ленина, д.63А Здание Суздальских торговых рядов"));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем страницу Магазины ", () -> {
+            open("/about/shops/");
+        });
+
+        step("Выбираем Суздаль из выпадающего списка ", () -> {
+            $(".vi-buy-pick__item-name-default").click();
+            $("[data-city=suzdal]").click();
+        });
+
+        step("Проверяем адрес магазина в Суздадале ", () -> {
+            $$(".vi-buy-city__block-shop-contact")
+                    .findBy(text("Адрес"))
+                    .sibling(0)
+                    .shouldHave(text("601293, Владимирская область, г. Суздаль, ул. Ленина, д.63А Здание Суздальских торговых рядов"));
+        });
+
     }
 
     @Test
     @Description("Алерт о безуспешной авторизации")
     void successfulRegistrationTest() {
-        open("/personal");
-        $("[name=USER_LOGIN]").setValue("test");
-        $("[name=USER_PASSWORD]").setValue("test");
-        $(".btn-primary[value=Войти]").click();
-        $(".alert-danger").shouldHave(text("Неверный логин или пароль."));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Заходим в ЛК ", () -> {
+            open("/personal");
+        });
+
+        step("Вводим " + TEXT + " в поля логина и пароля", () -> {
+            $("[name=USER_LOGIN]").setValue(TEXT);
+            $("[name=USER_PASSWORD]").setValue(TEXT);
+        });
+
+        step("Подтверждаем действия ", () -> {
+            $(".btn-primary[value=Войти]").click();
+        });
+
+        step("Проверяем алерт и текст его содержимого " + ALERT , () -> {
+            $(".alert-danger").shouldHave(text(ALERT));
+        });
+
 
     }
 
